d05e2317b054ea61e94abc30784569449c37c57c
Leon Crokiya <leoncrokiya13@gmail.com>
2026-07-07 14:56:51 +0200

    Add files via upload

---
+++ main2.0.py
@@ -0,0 +1,15 @@
+from nicegui import ui
+
+def greet_user():
+    name = name_input.value
+    label.text = f"hello {name}"
+
+name_input = ui.input(
+    label = "enter your name"
+)
+
+greet_button = ui.button("greet", on_click = greet_user)
+
+label = ui.label("")
+
+ui.run()
